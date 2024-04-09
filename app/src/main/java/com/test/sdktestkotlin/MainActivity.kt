324929674dbbe60c97f92eac742141679e6b0f25
JNYLee <2071013@hansung.ac.kr>
2024-04-09 13:45:52 +0900

    [fix]: fixed coords to draw route

--- app/src/main/java/com/test/sdktestkotlin/MainActivity.kt
+++ app/src/main/java/com/test/sdktestkotlin/MainActivity.kt
@@ -1,25 +1,9 @@
 package com.test.sdktestkotlin
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.skt.tmap.TMapView
-import android.graphics.Color
 import android.widget.FrameLayout
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
-import com.skt.tmap.TMapData
-import com.skt.tmap.TMapPoint
-import com.skt.tmap.overlay.TMapPolyLine
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import okhttp3.RequestBody
-import okhttp3.Response
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var tMapView: TMapView
@@ -29,30 +13,28 @@ class MainActivity : AppCompatActivity() {
 
         // TMapView 초기화 및 AppKey 인증 대기
         drawMap()
-        val response = routeApi()
-
-        for (tLine in response) {
-            tMapView.addTMapPolyLine(tLine)
-        }
+        routeApi()
     }
 
-    private fun routeApi(): Array<TMapPolyLine> {
+    private fun routeApi() {
         val transitManager = TransitManager(this@MainActivity)
 
         val routeRequest = RouteRequest(
-            startX = "126.926493082645",
-            startY = "37.6134436427887",
-            endX = "127.126936754911",
-            endY = "37.5004198786564",
+            startX = "126.98510375",
+            startY = "37.56648210",
+            endX = "127.01035102",
+            endY = "37.58178633",
             lang = 0,
             format = "json",
             count = 10
         )
-        var lines = emptyArray<TMapPolyLine>()
-            transitManager.getRoutes(routeRequest).observe(this, Observer { routeResponse ->
-                lines = RouteLine(routeResponse).drawRoute()
-        })
-        return lines
+        transitManager.getRoutes(routeRequest).observe(this@MainActivity) { routeResponse ->
+            val lines = RouteLine(routeResponse).drawRoute()
+
+            for (tLine in lines) {
+                tMapView.addTMapPolyLine(tLine)
+            }
+        }
     }
 
     private fun drawMap() {
--- app/src/main/java/com/test/sdktestkotlin/RouteLine.kt
+++ app/src/main/java/com/test/sdktestkotlin/RouteLine.kt
@@ -1,36 +1,44 @@
 package com.test.sdktestkotlin
 
 import android.graphics.Color
-import com.beust.klaxon.token.VALUE_TYPE.value
 import com.skt.tmap.TMapPoint
 import com.skt.tmap.overlay.TMapPolyLine
 
 class RouteLine(rr: RouteResponse) {
     private var routeResponse: RouteResponse = rr
-    public fun drawRoute(): Array<TMapPolyLine> {
-        var tmp = emptyArray<TMapPolyLine>()
+    fun drawRoute(): ArrayList<TMapPolyLine> {
+        val tmp = ArrayList<TMapPolyLine>()
         val itineraries = routeResponse.metaData?.plan?.itineraries?.filter { it -> it.pathType == 1 } // 지하철+도보
         if (itineraries != null) {
             val legs = itineraries[0].legs!!
+            var i = 1
             for (leg in legs) {
-                var t: TMapPolyLine = TMapPolyLine()
-                t.addLinePoint(TMapPoint(leg.start.lat, leg.start.lon))
+                var t = TMapPolyLine()
+                val tMapPoints = ArrayList<TMapPoint>()
+
+                tMapPoints.add(TMapPoint(leg.start.lat, leg.start.lon))
+
                 if (leg.mode == "WALK") {
-                    t.lineColor = Color.BLUE
                     for (step in leg.steps!!) {
                         val linestring = step.linestring.split(" ").map { it -> it.split(",") } // "111,222 333,444" -> [(111,222), (333,444)]
                             .associate { (key, value) -> key to value }
-                        linestring.forEach{ it -> t.addLinePoint(TMapPoint(it.key.toDouble(), it.value.toDouble()))}
+                        linestring.forEach{ it ->
+                            tMapPoints.add(TMapPoint(it.value.toDouble(), it.key.toDouble()))
+                        }
                     }
+                    tMapPoints.add(TMapPoint(leg.end.lat, leg.end.lon))
+                    t = TMapPolyLine("line"+(i++), tMapPoints)
+                    t.lineColor = Color.BLUE
                 } else if (leg.mode == "SUBWAY") {
-                    t.lineColor = Color.RED
                     val stationList = leg.passStopList!!.stationList!!
                     for (station in stationList) {
-                        t.addLinePoint(TMapPoint(station.lat.toDouble(), station.lon.toDouble()))
+                        tMapPoints.add(TMapPoint(station.lat.toDouble(), station.lon.toDouble()))
                     }
+                    tMapPoints.add(TMapPoint(leg.end.lat, leg.end.lon))
+                    t = TMapPolyLine("line"+(i++), tMapPoints)
+                    t.lineColor = Color.RED
                 }
-                t.addLinePoint(TMapPoint(leg.end.lat, leg.end.lon))
-                tmp.plus(t)
+                tmp.add(t)
             }
         }
         return tmp
